e51c0981f18a873c8a3ce141d97b50f2217b683c
unknown <daviddimas80@gmail.com>
2022-05-27 15:32:18 +0700

    get data from db trial

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
-
+const Schema  = mongoose.Schema;
+var stringcon='mongodb+srv://i18040:wearedead@cluster0.hw29l.mongodb.net/?retryWrites=true&w=majority/QuizDB'
+const conn = mongoose.createConnection(stringcon);
 const connect = async (e)=>{ 
-await mongoose.connect('mongodb+srv://i18040:wearedead@cluster0.hw29l.mongodb.net/?retryWrites=true&w=majority',{
+await mongoose.connect(stringcon,{
     // useNewUrlParse : true,
     // useUnifiedTopology:true,
     // useFindAndModify:false,
@@ -14,6 +16,22 @@ await mongoose.connect('mongodb+srv://i18040:wearedead@cluster0.hw29l.mongodb.ne
  })
 }
 
+const quizscheme = new Schema({
+     soal: String,
+     option1: String,
+     option2: String,
+     option3: String, 
+     option4: String,
+     jawaban :String
+    },{collection:'quiz'});
+
+var quiz=mongoose.model('quiz',quizscheme)
+
+const getQuiz = async (e)=>{
+    console.log(quiz.find())
+}
+
 module.exports={
-    connect:connect
+    connect:connect,
+    getQuiz:getQuiz
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const dbm=require('./db')
+const db = require('./db')
 dbm.connect()
+dbm.getQuiz()
 const PORT = process.env.PORT || 5000
 require('dotenv').config()
 const app = express()
