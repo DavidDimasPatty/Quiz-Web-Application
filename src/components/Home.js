d2dc8677e84025d4a33b2a24f0c53ebb0f42480a
unknown <daviddimas80@gmail.com>
2022-05-30 04:17:46 +0700

    persiapan production

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
       <body>
          
         <div className='buttons'>
-        <h1>Welcome To Quizez</h1>
+        <h1 style={{color:"white",fontSize:"20px"}}>Welcome To Quizez</h1>
         <Link to={`/category`} id='start'className='button-27'>Start</Link>
         <Link to={`/scoreboard`} id='scoreboard' className='button-27'>Score Board</Link>    
         <Link to={`/admin`} id='admin' className='button-27'>Admin</Link>     
